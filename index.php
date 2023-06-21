9bfa067b1b2d129be6ad48cc8eda99245e2aa13f
Ishan4u <mohamedishan44@gmail.com>
2023-06-21 23:03:33 +0530

    Add Search

--- index.php
+++ index.php
@@ -30,24 +30,24 @@ if (isset($_COOKIE['com_id'])) {
                 <div class="banner-content text-center">
                     <p>Find Jobs, Employment & Career Opportunities</p>
                     <h1>Drop Resume & Get Your Desire Job!</h1>
-                    <form class="banner-form">
+                    <form class="banner-form" method="post">
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Keyword:</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                    <input name="search_post" type="text" class="form-control" id="exampleInputEmail1"
                                         placeholder="Job Title">
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
                                     <label for="exampleInputEmail2">Location:</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail2"
-                                        placeholder="City or State">
+                                    <input name="search_location" type="text" class="form-control"
+                                        id="exampleInputEmail2" placeholder="City or State">
                                 </div>
                             </div>
                             <div class="col-md-4">
-                                <button type="submit" class="find-btn">
+                                <button type="submit" name="search_btn" class="find-btn">
                                     Find A Job
                                     <i class='bx bx-search'></i>
                                 </button>
@@ -71,13 +71,72 @@ if (isset($_COOKIE['com_id'])) {
 
 
 
+<?php
+if (isset($_POST['search_btn'])) { ?>
+    <!-- Start search result -->
+    <section class="company-section pt-100 pb-70">
+        <div class="container">
+            <div class="section-title text-center">
+                <h2>Search Result</h2>
+            </div>
+            <div class="row">
+                <!-- start card -->
 
-<section class="job-section pb-70">
+                <?php
+                $search_post = $_POST['search_post'];
+                $search_location = $_POST['search_location'];
+                $select_search = $conn->prepare("SELECT p.*, c.location FROM `post` p INNER JOIN `company` c ON p.com_id= c.com_id WHERE p.title LIKE '%{$search_post}%' AND c.location LIKE '%{$search_location}%' ");
+                $select_search->execute();
+                if ($select_search->rowCount() > 0) {
+                    while ($fetch_search = $select_search->fetch(PDO::FETCH_ASSOC)) {
+                        /* $post_id = $fetch_post['id'];
+
+                        $select_admin = $conn->prepare("SELECT * FROM `admin` WHERE id = ?");
+                        $select_admin->execute([$fetch_post['admin_id']]);
+                        $fetch_admin = $select_admin->fetch(PDO::FETCH_ASSOC); */?>
+
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="company-card">
+                                <div class="company-logo">
+                                    <a href="job-grid.html">
+                                        <img src="assets/img/top-company/1.png" alt="company logo">
+                                    </a>
+                                </div>
+                                <div class="company-text">
+                                    <h3>
+                                        <?php echo $fetch_search['title']; ?>
+                                    </h3>
+                                    <p>
+                                        <i class='bx bx-location-plus'></i>
+                                        <?php echo $fetch_search['location']; ?>
+                                    </p>
+                                    <!-- <a href="job-grid.html" class="company-btn">
+                                        25 Open Position
+                                    </a> -->
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php }
+                } else { ?>
+                    <div class="section-title text-center">
+                        <h3>Not Found</h3>
+                    </div>
+                <?php }
+} ?>
+            <!-- End card -->
+        </div>
+    </div>
+</section>
+<!-- End search result -->
+
+
+<section class="job-section pt-100 pb-70">
     <div class="container">
         <div class="section-title text-center">
             <h2>Jobs You May Be Interested In</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua. Quis ipsum suspendisse ultrices.</p>
+            <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Quis ipsum suspendisse ultrices.</p> -->
         </div>
         <div class="row">
 
